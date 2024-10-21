e0cfdd5338e18eb7c721afbf58214032044d7d0d
alswns6327 <alswns6327@naver.com>
2024-10-21 20:04:21 +0900

    style: 책 넘김 효과 적용

--- src/components/book/BookListTemplate.tsx
+++ src/components/book/BookListTemplate.tsx
@@ -24,10 +24,50 @@ const BookGridList = styled.div`
 
 const BookItem = styled.div`
   width: 130px;
-  height: 150px;
+  height: 180px;
   background-color: white;
   justify-self: end;
   position: relative;
+  perspective: 1000px;
+  &:hover > .firstPage {
+    z-index: 0;
+    transform: rotateY(-180deg);
+  }
+  &:hover > .firstPageBack {
+    z-index: 1;
+    transform: rotateY(-180deg);
+  }
+`;
+
+const BookItemPage = styled.div`
+  width: 100%;
+  height: 150px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  /* box-shadow: 0 2px 0 rgba(0, 0, 0, 0.3); */
+`;
+
+const BookItemFirstPage = styled(BookItemPage)`
+  z-index: 1;
+  transform-origin: left center;
+  transition-duration: 1s;
+`;
+
+const BookItemFirstPageBack = styled(BookItemFirstPage)`
+  z-index: 0;
+  transform-origin: left center;
+  transition-duration: 1s;
+  background-color: white;
+`;
+
+const BookItemSecondPage = styled(BookItemPage)`
+  z-index: 0;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
 `;
 
 const BookItemImg = styled.img`
@@ -77,7 +117,7 @@ const BookListTemplate = ({ bookInfoList }: BookListTemplateProps) => {
       <BookGridList>
         {bookInfoList?.map((bookInfo) => (
           <BookItem key={bookInfo.id}>
-            <Link to={"/chapter"}>
+            <BookItemFirstPage className="firstPage">
               <BookItemImg
                 src={
                   bookInfo.bookImgFile
@@ -85,6 +125,14 @@ const BookListTemplate = ({ bookInfoList }: BookListTemplateProps) => {
                     : `${process.env.PUBLIC_URL}/book.svg`
                 }
               />
+            </BookItemFirstPage>
+            <BookItemFirstPageBack className="firstPageBack" />
+            <Link to={"/chapter"}>
+              <BookItemSecondPage>
+                <span>chapter1</span>
+                <span>chapter2</span>
+                <span>chapter3</span>
+              </BookItemSecondPage>
             </Link>
             <BookUpdateButton>수정</BookUpdateButton>
             <BookDeleteButton>삭제</BookDeleteButton>
