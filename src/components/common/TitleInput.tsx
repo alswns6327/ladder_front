6de91020ad7e37c830217b8dedcc580e23b4a43a
alswns6327 <alswns6327@naver.com>
2024-11-04 21:04:11 +0900

    style: TitleInput 디자인 수정

--- src/components/common/TitleInput.tsx
+++ src/components/common/TitleInput.tsx
@@ -1,7 +1,12 @@
 import styled from "styled-components";
 import Input from "./Input";
 
-export const TitleInput = styled(Input)`
-  width: 85%;
+export const TitleInput = styled.input`
+  width: calc(100% - 10px);
+  height: 30px;
   text-align: center;
+  padding: 0 5px 0 5px;
+  border: none;
+  border-radius: 8px;
+  line-height: 30px;
 `;
